6f05c594b3a9e86321be01e3d8909eb49fe8ba7f
Pong Cheecharern <k.cheecharern@gmail.com>
2019-01-12 22:23:22 +0700

    added day three part two and tests

--- daythree/lib/daythree.ex
+++ daythree/lib/daythree.ex
@@ -9,7 +9,46 @@ defmodule Daythree do
     |> List.flatten
     |> Daythree.find_dup
   end
-  
+
+  def part_two(file) do
+    parse(file)
+    |> Enum.map(&(Daythree.split_val &1))
+    |> Enum.map(fn [id, left, top, width, length] -> 
+      {id, Daythree.get_coord([id, left, top, width, length]) |> MapSet.new()}
+    end)
+    |> Daythree.get_unique_id
+  end 
+
+  def get_unique_id(id_to_coords) do
+    Enum.reduce_while(id_to_coords, 0, fn {k,v}, acc ->  
+      case Daythree.do_get_uniq({k,v}, id_to_coords) do
+        {:not_found} -> {:cont, acc}
+        {:found, found_id} -> {:halt, found_id}
+      end
+    end)
+  end
+
+  def do_get_uniq({id, value}, rest) do
+    intersection_number = Enum.reduce(rest, 0, fn {k,v}, matches -> 
+      if MapSet.intersection(value, v) == MapSet.new([]), do: matches, else: matches + 1
+    end)
+    case intersection_number do
+      1 -> {:found, id}
+      _ -> {:not_found}
+    end
+  end
+
+  def get_coord([_,left, top, width, length]) do
+    {start_left, start_top} = {(left + 1), (top + 1)}
+    Enum.reduce(0..width - 1, [], fn w, acc ->
+      width_values = Enum.reduce(0..length - 1, [], fn l, acc -> 
+        [{w + start_left, l + start_top} | acc]
+      end)
+      [width_values | acc]
+    end) 
+    |> List.flatten
+  end 
+
   def find_dup(list) do
     {matched, _} = Enum.reduce(list, {MapSet.new(), MapSet.new()}, fn {x, y}, {matched, unmatched} ->
         if MapSet.member?(unmatched, {x, y}) do
@@ -21,27 +60,9 @@ defmodule Daythree do
     MapSet.size(matched)
   end
 
-  def get_coord({left, top, width, length}) do
-    {start_left, start_top} = {(left + 1), (top + 1)}
-    Enum.reduce(0..width - 1, [], fn w, acc ->
-      width_values = Enum.reduce(0..length - 1, [], fn l, acc -> 
-        [{w + start_left, l + start_top} | acc]
-      end)
-      [width_values | acc]
-    end) 
-    |> List.flatten
-  end 
-
   def split_val(string) do
-    [string, start] = String.split(string, ": ") 
-    Daythree.split_val(string, start) 
-  end
-  
-  def split_val(string, area) do
-    [string, start] = String.split(string, "@ ")
-    [left, top] = String.split(start, ",")
-    [width, length] = String.split(area, "x")
-    {String.to_integer(left),String.to_integer(top),String.to_integer(width), String.to_integer(length)}
+    [_, id, left, top, width, length] = String.split(string,["#"," @ ",",",": ","x"])
+    Enum.map([id, left, top, width, length], &String.to_integer/1)
   end
 
   def parse(file) do
--- daythree/test/daythree_test.exs
+++ daythree/test/daythree_test.exs
@@ -2,7 +2,19 @@ defmodule DaythreeTest do
   use ExUnit.Case
   doctest Daythree
 
-  test "should output the right square inches" do
+  test "should output the right square inches for test input" do
     assert Daythree.part_one("input1.txt") == 4
+  end
+
+  test "should output the right square inches for real input" do
+    assert Daythree.part_one("input.txt") == 97218
+  end
+
+  test "should find unique claim for test input" do
+    assert Daythree.part_two("input1.txt") == 3
+  end
+
+  test "should find the unique claim" do
+    assert Daythree.part_two("input.txt") == 717
   end
 end
