b3167a49caffab7b182d3f09d77ea009baaa0127
Pong Cheecharern <k.cheecharern@gmail.com>
2019-06-10 21:41:06 +0700

    working first example

--- dayfive/lib/dayfive.ex
+++ dayfive/lib/dayfive.ex
@@ -11,12 +11,16 @@ defmodule Dayfive do
 
   def part_one(file) do
     parse(file)
-    |> scan()
-    |> react()
+    |> run_loop
   end
 
   # running scanning and reacting the polymer until no
-  def run_loop() do
+  def run_loop({:ok, list}) do
+    scan(list) |> react() |> run_loop()
+  end
+  
+  def run_loop(list) do
+    scan(list) |> react() |> run_loop()
   end
 
   # start going through list from head, returns the first valid match and the current list
@@ -26,25 +30,15 @@ defmodule Dayfive do
         #if last_value is still nil, do nothing- sets last value to current
         {:error, :nil_val} -> {:cont, {value, last_valid_polymers, index + 1}}
         #if last_value is not nil, check if value and last_val is a valid_polymer_pair, if no sets last val to current, 
-        {:error, :not_matched} -> {:cont, {value, last_valid_polymers, index + 1}}
-        # if ok, check the next closest value
-        {:ok } -> {:cont, {value, [last_value, value], index + 1}}
-        {:ok, :keep_going} -> {:cont, {value, last_valid_polymers ++ [value], index + 1}}
-        {:ok, :stop} -> {:halt, {last_valid_polymers, index - 1}}
+        {:ok, :keep_going} -> {:cont, {value, last_valid_polymers, index + 1}}
+
+        {:ok} -> {:halt, {[last_value, value], index - 1}}
       end
     end)
     #returns res and first polymer_match
     {res, length(first_match), index}
   end
-
-  def react({list, 0, index}), do: {:ok, list} 
-
-  def react({list, units, index}) do
-    new_list = List.delete_at(list, index)
-    react({new_list, units - 1, index - 1})
-  end
   
-
   def valid_polymer_pair(unit1, {nil, nil, 0}), do: {:error, :nil_val}
 
   # validate first pair
@@ -54,20 +48,30 @@ defmodule Dayfive do
     case val1 - val2 do 
       32 -> {:ok}
       - 32 -> {:ok}
-      _ -> {:error, :not_matched}
+      _ -> {:ok, :keep_going}
     end
   end
 
-  # validate already matched pair with next value
-  def valid_polymer_pair(unit_1, {unit_2, prev_match, _}) do
-    <<val1::utf8>> = unit_1
-    <<val2::utf8>> = unit_2
-    case val1 - val2 do 
-      32 -> {:ok, :keep_going}
-      - 32 -> {:ok, :keep_going}
-      _ -> {:ok, :stop} # done, the prev match is last valid match
-    end
+  def react({list, 0, index}) do
+   IO.inspect(list)
+   {:ok, list} 
   end
+
+  def react({list, units, index}) do
+    new_list = List.delete_at(list, index)
+    react({new_list, units - 1, index})
+  end
+
+  # # validate already matched pair with next value
+  # def valid_polymer_pair(unit_1, {unit_2, prev_match, _}) do
+  #   <<val1::utf8>> = unit_1
+  #   <<val2::utf8>> = unit_2
+  #   case val1 - val2 do 
+  #     32 -> {:ok, :keep_going}
+  #     - 32 -> {:ok, :keep_going}
+  #     _ -> {:ok, :stop} # done, the prev match is last valid match
+  #   end
+  # end
 
   # returns a list of graphemes
   def parse(file) do
